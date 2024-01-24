c1f5acacc6fc41aa7701d39adc3df7e4014a60ee
Valentin Lopez <valentinlopezandres@gmail.com>
2024-01-24 18:57:05 -0300

    Lock rotation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:elements_detector/ipInput.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
 
   // Ensure that plugin services are initialized so that `availableCameras()`
   WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
 
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
